907da22b7b8c6aed1cd61ba721164e38cc07c73c
Daniel Mulvad <daniel@dmulvad.com>
2023-04-06 18:34:54 -0700

    LOG_LEVEL env

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use env_logger::Builder;
 use tda_server::server;
 
 #[tokio::main]
 async fn main() {
-    env_logger::init();
+    Builder::new().parse_env("LOG_LEVEL").init();
     let server = server::Server::new();
     server.start(None).await;
 }
