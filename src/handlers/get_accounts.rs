2ef1f9811213c6bd67d4cdd3eb7315036c003f96
Daniel Mulvad <daniel@dmulvad.com>
2023-04-06 20:02:57 -0700

    GetAccountsResponse

--- src/handlers/get_accounts.rs
+++ src/handlers/get_accounts.rs
@@ -1,15 +1,21 @@
+use crate::{
+    td_client::{GetAccountsResponse, TDAmeritradeClientAccounts},
+    AppState,
+};
 use axum::{extract::State, response::IntoResponse, Json};
 use axum_extra::extract::CookieJar;
 use hyper::StatusCode;
-use serde_json::Value;
-
-use crate::{td_client::TDAmeritradeClientAccounts, AppState};
 
 pub async fn get_accounts(jar: CookieJar, State(state): State<AppState>) -> impl IntoResponse {
     let access_token_cookie = jar.get("access_token");
     let token = match access_token_cookie {
         Some(cookie) => cookie.value(),
-        None => return (StatusCode::UNAUTHORIZED, Json(Value::default())),
+        None => {
+            return (
+                StatusCode::UNAUTHORIZED,
+                Json(GetAccountsResponse::default()),
+            )
+        }
     };
     let token_response = state.td_client.get_accounts(token).await;
     (StatusCode::OK, Json(token_response))
