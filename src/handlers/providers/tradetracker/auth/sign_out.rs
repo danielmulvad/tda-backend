b1ea48be07183ad7e8585c838f740c6b70272d83
Daniel Mulvad <daniel@dmulvad.com>
2023-04-21 03:59:38 -0700

    redirect to login

--- src/handlers/providers/tradetracker/auth/sign_out.rs
+++ src/handlers/providers/tradetracker/auth/sign_out.rs
@@ -1,12 +1,12 @@
-use axum::response::IntoResponse;
+use crate::utils::{cookie, get_base_url};
+use axum::response::{IntoResponse, Redirect};
 use axum_extra::extract::CookieJar;
-use hyper::StatusCode;
-
-use crate::utils::cookie;
 
 pub async fn auth_sign_out(jar: CookieJar) -> impl IntoResponse {
     let access_token_cookie = cookie::delete_access_token();
     let refresh_token_cookie = cookie::delete_refresh_token();
     let jar = jar.add(access_token_cookie).add(refresh_token_cookie);
-    (StatusCode::OK, jar)
+    let mut base_url = get_base_url();
+    base_url.set_path("/login/");
+    (jar, Redirect::temporary(base_url.as_str()))
 }
