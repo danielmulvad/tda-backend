7c313e030236d406ddb84cca3af9d90ade0315e7
Daniel Mulvad <daniel@dmulvad.com>
2023-05-16 04:53:48 -0700

    public tda callback url

--- src/router.rs
+++ src/router.rs
@@ -29,10 +29,10 @@ impl Router {
             .route("/:account_id/get_orders", get(tda::get_orders))
             .route("/auth/providers/tda", get(tda::auth::get_authorization_url))
             .route("/auth/providers/tda", post(tda::auth_tda_refresh_token))
-            .route("/auth/callback/tda", get(tda::auth::callback::tda))
             .route_layer(axum::middleware::from_fn_with_state(app_state.clone(), middleware::jwt::auth));
         let public_routes = axum::Router::new()
             .route("/", get(handlers::root))
+            .route("/auth/callback/tda", get(tda::auth::callback::tda))
             .route("/auth/providers/tradetracker", post(tradetracker::auth::auth_tradetracker_refresh_token))
             .route("/auth/providers/tradetracker/signup", post(tradetracker::auth::auth_sign_up_with_email_password))
             .route("/auth/providers/tradetracker/signin", post(tradetracker::auth::auth_sign_in_with_email_password))
